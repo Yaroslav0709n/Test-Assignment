36cb0243784ede15c9fbccebc609b200d895b580
Ярослав <vizichkanich0709@gmail.com>
2023-08-16 22:21:41 +0300

    initial commit

--- Test Assignment/wwwroot/js/site.js
+++ Test Assignment/wwwroot/js/site.js
@@ -1,4 +1,64 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    $('#saveBtn').click(function () {
+        var message = $('#message').val();
+
+        $.post('/Home/Message', { message: message }, function (data) {
+            if (data.success) {
+                alert(data.message);
+                location.reload();
+            }
+        });
+    });
+
+    loadAllMessages();
+    loadCurrentUserMessages()
+
+    function loadCurrentUserMessages() {
+        $.ajax({
+            url: '/Home/GetCurrentUserMessages',
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var tableBody = $('#messageTableBody1');
+                tableBody.empty();
+
+                $.each(data, function (index, message) {
+                    var row = $('<tr>');
+                    row.append('<td>' + (index + 1) + '</td>');
+                    row.append('<td  class="name-collumn">' + message.userName + '</td>');
+                    row.append('<td  class="message-collumn">' + message.message + '</td>');
+                    row.append('<td>' + (new Date(message.time)).toLocaleString() + '</td>');
+                    tableBody.append(row);
+                });
+            },
+            error: function () {
+                console.log('Error loading data.');
+            }
+        });
+    }
+
+    function loadAllMessages() {
+        $.ajax({
+            url: '/Home/GetAllUsersMessages',
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var tableBody = $('#messageTableBody2');
+                tableBody.empty();
+
+                $.each(data, function (index, message) {
+                    var row = $('<tr>');
+                    row.append('<td>' + (index + 1) + '</td>');
+                    row.append('<td  class="name-collumn">' + message.userName + '</td>');
+                    row.append('<td  class="message-collumn">' + message.message + '</td>');
+                    row.append('<td>' + (new Date(message.time)).toLocaleString() + '</td>');
+                    tableBody.append(row);
+                });
+            },
+            error: function () {
+                console.log('Error loading data.');
+            }
+        });
+    }
+});
