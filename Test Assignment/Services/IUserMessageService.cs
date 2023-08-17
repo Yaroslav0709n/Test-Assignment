727624aadd0e262c123a1966b9bedb6dd1c1a886
Ярослав <vizichkanich0709@gmail.com>
2023-08-17 21:03:45 +0300

    create access to db

---
+++ Test Assignment/Services/IUserMessageService.cs
@@ -0,0 +1,11 @@
+﻿using Test_Assignment.Models;
+
+namespace Test_Assignment.Services
+{
+    public interface IUserMessageService
+    {
+        Task<List<User>> GetAllUsersMessagesAsync();
+        Task<List<User>> GetCurrentUserMessagesAsync();
+        Task AddMessageAsync(string message);
+    }
+}
---
+++ Test Assignment/Services/UserMessageService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Assignment.Context;
+using Test_Assignment.Models;
+using Test_Assignment.Common;
+
+namespace Test_Assignment.Services
+{
+    public class UserMessageService : IUserMessageService
+    {
+        private readonly TestDbContext _dbContext;
+        public UserMessageService(TestDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        async public Task AddMessageAsync(string message)
+        {
+            var newUser = new User
+            {
+                UserName = CommonOptions.currentMachineName,
+                Message = message,
+                Time = DateTime.Now,
+            };
+
+            if(newUser != null)
+            {
+                await _dbContext.Users.AddAsync(newUser);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        async public Task<List<User>> GetAllUsersMessagesAsync()
+        {
+            var messages = await _dbContext.Users.ToListAsync();
+            if (messages.Count >= 20)
+            {
+                var oldestMessage = messages[0];
+                _dbContext.Users.Remove(oldestMessage);
+                await _dbContext.SaveChangesAsync();
+            }
+            return messages;
+        }
+
+        async public Task<List<User>> GetCurrentUserMessagesAsync()
+        {
+            var filteredMessages = await _dbContext.Users
+                .Where(u => u.UserName == CommonOptions.currentMachineName)
+                .ToListAsync();
+
+            if (filteredMessages.Count >= 10)
+            {
+                var oldestMessage = filteredMessages[0];
+                _dbContext.Users.Remove(oldestMessage);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return filteredMessages;
+        }
+    }
+}
