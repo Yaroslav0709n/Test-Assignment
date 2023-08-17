1eed6414d8c08761b5a55d39299c5f7094394340
Ярослав <vizichkanich0709@gmail.com>
2023-08-17 21:02:24 +0300

    add context

---
+++ Test Assignment/Context/TestDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Assignment.Models;
+
+namespace Test_Assignment.Context
+{
+    public class TestDbContext:DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
